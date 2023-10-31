a1920ef07164bf4e0667cae0be80bff3af7c5c09
gennadii garshin <gippi122221@gmail.com>
2023-10-31 11:06:43 +0800

    modify resultset populating

--- bot/db_service.py
+++ bot/db_service.py
@@ -54,35 +54,3 @@ class MongoDBService:
 
     result = self.db.sample_collection.aggregate(pipeline).next()
     return result
-
-
-  async def run_aggregation_details(self, dt_1, dt_2, dt_type):
-
-    date_range = {
-        "$gte": dt_1,
-        "$lte": dt_2
-    }
-
-    date_trunc_unit = dt_type
-
-    pipeline = [
-        {
-            "$match": {
-                "dt": date_range
-            }
-        },
-        {
-            "$group": {
-                "_id": { "$dateTrunc": { "date": "$dt", "unit": date_trunc_unit } },
-                "total_sum": { "$sum": "$value" }
-            }
-        },
-        {
-            "$sort": {
-                "_id": 1
-            }
-        },
-    ]
-
-    result = list(self.db.sample_collection.aggregate(pipeline))
-    return result
--- bot/run.py
+++ bot/run.py
@@ -21,6 +21,27 @@ db_name = os.getenv('DB_NAME')
 
 dp = Dispatcher()
 
+def get_ranges(dt_1, dt_2, group_type):
+  date_range = []
+
+  if group_type == "hour":
+    interval = timedelta(hours=1)
+  elif group_type == "day":
+    interval = timedelta(days=1)
+  elif group_type == "week":
+      interval = timedelta(weeks=1)
+  elif group_type == "month":
+    interval = relativedelta(months=1)
+  else:
+    raise ValueError("Invalid group_type. Supported group_type are 'hour', 'day', 'week' or 'month'.")
+
+  current_date = dt_1
+  while current_date <= dt_2:
+    date_range.append(current_date)
+    current_date += interval
+  
+  return date_range
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
   await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
@@ -37,39 +58,31 @@ async def process_input(message: types.Message) -> None:
     dt_2 = datetime.strptime(input_data['dt_upto'], '%Y-%m-%dT%H:%M:%S')
     dt_type = input_data['group_type']
 
-    date_range = []
-
-    if dt_type == "hour":
-      interval = timedelta(hours=1)
-    elif dt_type == "day":
-      interval = timedelta(days=1)
-    elif dt_type == "month":
-      interval = relativedelta(months=1)
-    else:
-      raise ValueError("Invalid dt_type. Supported dt_type are 'hour', 'day', or 'month'.")
-
-    current_date = dt_1
-    while current_date <= dt_2:
-      date_range.append(current_date)
-      current_date += interval
+    ranges = get_ranges(dt_1, dt_2, dt_type)
 
     db_service = MongoDBService(db_url, db_name)
 
-    result = await db_service.run_aggregation_details(dt_1, dt_2, dt_type)
+    result = await db_service.run_aggregation(dt_1, dt_2, dt_type)
     
     db_service.close_connection()
 
-    result_data = []
-    for dt in date_range:
-      fobj = next((obj for obj in result if obj["_id"] == dt), {"_id": dt, "total_sum": 0})
-      result_data.append(fobj)
+    dataset = result['dataset']
+    labels = result['labels']
+
+    new_ds = []
+    new_labels = []
     
-    labels = [item['_id'].strftime('%Y-%m-%dT%H:%M:%S') for item in result_data]
-    dataset = [item['total_sum'] for item in result_data]
+    for idx, x in enumerate(ranges):
+      new_labels.append(datetime.strftime(x, '%Y-%m-%dT%H:%M:%S'))
+      if x in labels:
+        index = labels.index(x)
+        new_ds.append(dataset[index])
+      else:
+        new_ds.append(0)
 
     output_data = {
-      "dataset": dataset,
-      "labels": labels,
+      'dataset': new_ds,
+      'labels': new_labels
     }
 
     result_json = json.dumps(output_data, default=str)
